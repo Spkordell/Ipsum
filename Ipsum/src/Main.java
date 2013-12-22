485d4cb3677ea603be15ad5199afae2c55cf15f4
Spkordell <mask@mask>
2013-12-21 21:44:16 -0500

    Initial commit

---
+++ Ipsum/src/Main.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Main {
+	private static JFrame frame;
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        frame = new JFrame("Ipsum");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        //Display the window.
+        frame.setPreferredSize(new Dimension(1000,600));
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
